5b13e6823f592ecff2b5337f51142105d8a7495c
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2017-10-26 15:47:26 -0200

    First template

--- src/urls.py
+++ src/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+#Views from apps
+from src.apps.core import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='homepage')
 ]
