29462f5ea5fc9a484f75a986d6cd6dc7c5739920
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2017-10-25 22:24:47 -0200

    python-decouple to hide important variables

--- src/settings.py
+++ src/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qun%z!%8tbb9w@-zfqzjlr9vba&vpv7=$8-@k+*gcc)0+_iwo1'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
