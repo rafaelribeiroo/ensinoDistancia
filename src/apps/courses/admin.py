42a43cabd6040493c0e2af0f57dec9777d8a434e
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2017-11-09 23:47:59 -0200

    Admin III, SlugField

--- src/apps/courses/admin.py
+++ src/apps/courses/admin.py
@@ -6,3 +6,4 @@ from .models import Course
 class CourseAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'start_date', 'created_at']
     search_fields = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
