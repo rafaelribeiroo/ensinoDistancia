8b948b73848dfc4586c19341bf95e0962d231f57
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2017-11-13 22:46:22 -0200

    Email: Configurações iniciais no settings

--- src/settings.py
+++ src/settings.py
@@ -133,3 +133,15 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'src', 'media')
+
+# E-mails
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'Rafael Ribeiro <pereiraribeirorafael@gmail.com>'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+
+config('SECRET_KEY')
