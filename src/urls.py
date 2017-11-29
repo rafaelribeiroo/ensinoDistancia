5d3fddd6e015c20ee810c05ae3436d267520f2d3
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2017-11-28 22:46:59 -0200

    Best practices on URLs

--- src/urls.py
+++ src/urls.py
@@ -5,14 +5,17 @@ from django.conf.urls.static import static
 
 # Views from apps
 # from src.apps.core import views
+from src.apps.core import urls as core_urls
+from src.apps.courses import urls as courses_urls
+
 
 urlpatterns = [
     # R: indica que a string vai ser uma expressão regular, então caso tenha \n (que no .py significa quebra de linha, vai significar outra coisa, com base no REGEX)
     url(r'^admin/', admin.site.urls),
     # Porque definir um namespace para um include de URL?
     # Caso eu tenha dois namespaces iguais, definirei cada um pelo nome do include antes, exemplo> core:home
-    url(r'^', include('src.apps.core.urls', namespace='core')),
-    url(r'^curso[s]/', include('src.apps.courses.urls', namespace='courses')),
+    url(r'^', include(core_urls, namespace='core')),
+    url(r'^curso[s]/', include(courses_urls, namespace='courses')),
 ]
 
 if settings.DEBUG:
