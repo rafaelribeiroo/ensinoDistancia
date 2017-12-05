fd0388c677c881b0d419f6d3f6faabcce6f915cb
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2017-12-04 22:41:46 -0200

    NoReverseMatch, i will fix it later

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url  # include
-from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    url(r'^entrar/$', views.login,
-        {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^entrar/$', auth_views.LoginView.as_view(
+        template_name='login.html'), name='login'),
 ]
