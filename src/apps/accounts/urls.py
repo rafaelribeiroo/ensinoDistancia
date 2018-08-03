7687c427f2d7769c3cafdd837fef0e57e8762a0a
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-08-03 13:18:34 -0300

    Registrar

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView  # , LogoutView
 # from django.contrib.auth import views as auth_views
+from src.apps.accounts.views import register
+
 
 app_name = 'accounts'
 urlpatterns = [
     path('entrar/', LoginView.as_view(
         template_name='login.html'), name='login'),
+    path('registrar/', register, name='register'),
 ]
